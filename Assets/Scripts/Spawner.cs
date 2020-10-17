2416008f8cae523ee332ff72775852e5e3ef78c9
Andrew Liu <aliu.13119@gmail.com>
2020-10-17 15:11:32 -0700

    Changed shooting to stop at mouse and shoot with space, Added zombie/devil scripts with HP/ability to shoot fireballs, "spawner" for testing

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Use by attaching to some game object to spawn zombie/devil prefabs, helpful for testing
+
+public class Spawner : MonoBehaviour
+{
+
+    public float period;
+    public float devilRate;
+
+    [SerializeField] private Transform zombie;
+    [SerializeField] private Transform devil;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.timer += Time.deltaTime;
+        if (this.timer >= period)
+        {
+            if (Random.Range(0f, 1.0f) < devilRate)
+            {
+                Instantiate(devil, transform.position, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(zombie, transform.position, Quaternion.identity);
+            }
+            this.timer = 0;
+        }
+    }
+}
