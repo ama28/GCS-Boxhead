813cdac40d55afa8a791615c156e58994b6d323a
Will Ozeas <willatme@me.com>
2020-11-09 21:55:03 -0800

    Added Music Looping Script and Added Gunshot Noises
    All changes in Music Experiment Scene

---
+++ Assets/Scripts/MusicLooper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicLooper : MonoBehaviour
+{
+    [SerializeField]
+	public AudioSource audio;
+	[SerializeField]
+	public int loopSamples;
+	[SerializeField]
+	public int startDelay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio.Play();
+        audio.time = 179;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(audio.timeSamples > loopSamples) {
+        	audio.timeSamples = startDelay;
+        }
+    }
+}
