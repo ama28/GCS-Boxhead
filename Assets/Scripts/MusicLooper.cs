2a48e2ee1584bb9bcf37e1966eb79ce9c33634d1
Will Ozeas <willatme@me.com>
2020-11-11 22:48:46 -0800

    Added music to Player prefab
    - Edited AudioLooper to automatically set loop times and audio files based on scene name
    - Added gunshot to Player Prefab

--- Assets/Scripts/MusicLooper.cs
+++ Assets/Scripts/MusicLooper.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicLooper : MonoBehaviour
 {
     [SerializeField]
 	public AudioSource audio;
+    [SerializeField]
+    public AudioClip bossMusic;
+    [SerializeField]
+    public AudioClip levelMusic;
+    //unserialize these 2 once timing is determined
 	[SerializeField]
 	public int loopSamples;
 	[SerializeField]
@@ -13,6 +19,14 @@ public class MusicLooper : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if(SceneManager.GetActiveScene().name == "Boss") {
+            audio.clip = bossMusic;
+            //Add in boss music loop timing here!
+        } else {
+            audio.clip = levelMusic;
+            loopSamples = 8173701;
+            startDelay = 594200;
+        }
         audio.Play();
     }
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gunshot.volume = 0.75f;
+        gunshot.volume = 0.5f;
     }
 
 
