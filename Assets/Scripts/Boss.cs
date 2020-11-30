b0f0bde449e84fff4a4d2afdbfc2edeecf140065
Michael P <mvpoirier2@yahoo.com>
2020-11-29 23:53:15 -0500

    Redid Boss Rotation script and did Swipe attack
    In order for me to to even do the swipe attack I bassicly had to redo the whole dog and its roation script.

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -7,13 +7,19 @@ public class Boss : MonoBehaviour
 {
     private Transform target;
     public Animator animator;
-
+    public GameObject Neck;
     public Int health;
+
+    public float Neckspeed;
 
     public float shootPeriod;
     private float timer;
     [SerializeField] private Transform fireball;
     public Transform fireballPosition;
+
+    public Vector3 newDirection;
+
+    public bool inRange;
 
     void Start()
     {
@@ -24,16 +30,30 @@ public class Boss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("Swipe", inRange);
+
         this.timer += Time.deltaTime;
 
+
+        float neckROT = Neckspeed * Time.deltaTime;
+
         Vector2 dir = (target.position - transform.position).normalized;
+
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, dir, neckROT, 0.0f);
+
+
+
+        Neck.transform.eulerAngles = new Vector3(0, 0, getAngleFromVector(dir)+90);
+
+
+
         if (this.timer >= this.shootPeriod)
         {
             Shoot(dir);
             this.timer = 0;
         }
-        //animator.SetFloat("Horizontal", dir.x);
-        //animator.SetFloat("Vertical", dir.y);
+       
+        
     }
 
     void Shoot(Vector3 dir)
@@ -42,5 +62,40 @@ public class Boss : MonoBehaviour
         bulletTransform.GetComponent<Fireball>().setup(dir);
     }
 
+
+    public float getAngleFromVector(Vector3 dir)
+    {
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+        return n;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+
+        {
+
+            inRange = true;
+
+
+        }
+     
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+
+        {
+
+            inRange = false;
+
+
+        }
+
+    }
 
 }
