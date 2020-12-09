8cd1c81637cb1cdeda8a817dfbf7fbca2cf24b54
davidfan168 <davidfan168@hotmail.com>
2020-12-09 02:40:19 -0500

    modified the UI

---
+++ Assets/Scripts/KeyImage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class KeyImage : MonoBehaviour
+{
+    public Sprite normalKey;
+    public Sprite bossKey;
+
+    public Image keySprite;
+
+    string sceneName;
+    void Awake()
+    {
+        SceneManager.activeSceneChanged += ChangedActiveScene;
+    }
+
+    void ChangedActiveScene(Scene current, Scene next)
+    {
+        sceneName = next.name;
+
+        if (sceneName == "floor_1")
+        {
+            keySprite.sprite = normalKey;
+        }
+        else if (sceneName == "floor_2")
+        {
+            keySprite.sprite = bossKey;
+        }
+        else
+        {
+            keySprite.enabled = false;
+        }
+    }
+}
